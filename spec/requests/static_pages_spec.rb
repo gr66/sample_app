081f9c89cf2d6108ea4b4a45d1f009d4e2a521c4
Gregory Kravchenko <gpk@anahoret.com>
2015-06-16 17:43:04 +0300

    Chapter 4

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.feature "StaticPages", :type => :feature do
-  let(:base_title) { 'Ruby on Rails Tutorial Sample App |' }
+  let(:base_title) { 'Ruby on Rails Tutorial Sample App' }
   describe "Home page" do
     scenario "should have the content 'Sample App" do
       visit "/static_pages/home"
@@ -10,7 +10,11 @@ RSpec.feature "StaticPages", :type => :feature do
     end
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title("#{base_title} Home")
+      expect(page).to have_title("#{base_title}")
+    end
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
 
@@ -21,7 +25,7 @@ RSpec.feature "StaticPages", :type => :feature do
     end
     it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title("#{base_title} Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -32,7 +36,7 @@ RSpec.feature "StaticPages", :type => :feature do
     end
     it "should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title("#{base_title} About Us")
+      expect(page).to have_title("#{base_title} | About Us")
     end
   end
 
@@ -43,7 +47,7 @@ RSpec.feature "StaticPages", :type => :feature do
     end
     it "should have the right title" do
       visit '/static_pages/contact'
-      expect(page).to have_title("#{base_title} Contact")
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
 end
